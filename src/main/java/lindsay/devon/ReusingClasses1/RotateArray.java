1a0c6377792b1b4be3c6ab4f6ca1f22080a68859
Devon Lindsay <johnde02@gmail.com>
2016-09-15 17:22:39 -0400

    update 3 bank account

---
+++ src/main/java/lindsay/devon/ReusingClasses1/RotateArray.java
@@ -0,0 +1,30 @@
+package lindsay.devon.ReusingClasses1;
+import java.awt.List;
+import java.lang.reflect.Array;
+/**
+ * Created by devonlindsay on 9/15/16.
+ */
+public class RotateArray extends List{
+
+/* Write a function that rotates a list by k elements.
+For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2].
+Try solving this without creating a copy of the list. How many swap or move operations do you need?
+
+In your main class, initialize your custom List with values
+In your main class call the method that rotates your array
+Print original and rotated List values to screen */
+
+String[] myList = new String[5];
+    myList[0] = "Dogs";
+    myList[1] = "Cats";
+    myList[2] = "Fish";
+    myList[3] = "Horses";
+    myList[4] = "Penguins";
+
+public void rotateList(Array[] beta, int k) {
+    for(int i = 0; i < beta.length; i + k ) {
+
+    }
+}
+
+}
